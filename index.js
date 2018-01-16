8cb6dba3ed45c8570e7ac4bd05eb1dcb52a4e93d
Sam Selikoff <sam.selikoff@gmail.com>
2018-01-16 17:38:37 -0500

    Improve force sync relationships

--- index.js
+++ index.js
@@ -2,5 +2,42 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-data-storefront'
+  name: 'ember-data-storefront',
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  included() {
+    let app;
+
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === 'function') {
+      app = this._findHost();
+    } else {
+      // Otherwise, we'll use this implementation borrowed from the _findHost()
+      // method in ember-cli.
+      let current = this;
+      do {
+        app = current.app || app;
+      } while (current.parent.parent && (current = current.parent));
+    }
+
+    this.app = app;
+    this.addonConfig = this.app.project.config(app.env)['ember-data-storefront'] || {};
+
+    let validOptions = [ 'rewrite', undefined ];
+    if (!validOptions.includes(this.addonConfig.forceSyncRelationships)) {
+      throw `[ Ember Data Storefront ] Invalid config option for forceSyncRelationships. You passed "${this.addonConfig.forceSyncRelationships}", must be one of ${validOptions.map(option => `"${option}"`).join(", ")}.`;
+    }
+
+    if (this.addonConfig.forceSyncRelationships === 'rewrite') {
+      app.options = app.options || {};
+      app.options.babel6 = app.options.babel6 || {};
+      app.options.babel6.plugins = app.options.babel6.plugins || [];
+      app.options.babel6.plugins.push(require('./force-sync-relationships-plugin'));
+    }
+  }
+
 };
---
+++ node-tests/force-sync-relationships-plugin/has-many-as-identiifer-test.js
@@ -0,0 +1,109 @@
+/* eslint-env node */
+var babel = require('babel-core');
+var stripIndent = require('strip-indent');
+var ForceSynceRelationshipsPlugin = require('../../force-sync-relationships-plugin');
+var module = QUnit.module;
+var test = QUnit.test;
+
+function testOriginalToTransformed(description, original, transformed) {
+  return test(description, function(assert) {
+    let strippedOriginal = stripIndent(original);
+    let strippedTransformed = stripIndent(transformed);
+    var result = babel.transform(strippedOriginal, {
+      plugins: [ ForceSynceRelationshipsPlugin ],
+    });
+
+    assert.equal(result.code.trim(), strippedTransformed.trim());
+  });
+}
+
+module('hasMany as Identifier', function() {
+
+  testOriginalToTransformed('it works when called with no arguments',
+    `
+      export default Model.extend({
+
+        posts: hasMany()
+
+      });
+    `,
+    `
+      export default Model.extend({
+
+        posts: hasMany({
+          async: false
+        })
+
+      });
+  `);
+
+  testOriginalToTransformed('it works when called with a model name',
+    `
+      export default Model.extend({
+
+        posts: hasMany('post')
+
+      });
+    `,
+    `
+      export default Model.extend({
+
+        posts: hasMany('post', {
+          async: false
+        })
+
+      });
+    `
+  );
+
+  testOriginalToTransformed('it merges with options other than async',
+    `
+      export default Model.extend({
+
+        posts: hasMany({ inverse: 'foo' })
+
+      });
+    `,
+    `
+      export default Model.extend({
+
+        posts: hasMany({ inverse: 'foo', async: false
+        })
+
+      });
+    `
+  );
+
+  test('it throws when it sees async: true', function(assert) {
+    let strippedOriginal = stripIndent(`
+      export default Model.extend({
+
+        posts: hasMany({ async: true })
+
+      });
+    `);
+
+    assert.throws(() => {
+      babel.transform(strippedOriginal, {
+        plugins: [ ForceSynceRelationshipsPlugin ],
+      });
+    });
+  });
+
+  test('it throws when it sees async: false', function(assert) {
+    let strippedOriginal = stripIndent(`
+      export default Model.extend({
+
+        posts: hasMany({ async: false })
+
+      });
+    `);
+
+    assert.throws(() => {
+      babel.transform(strippedOriginal, {
+        plugins: [ ForceSynceRelationshipsPlugin ],
+      });
+    });
+  });
+
+});
