ddaf84f66acd29281580863c8a0c98329ff8d2c0
Ryan Toronto <ryanto@gmail.com>
2019-12-13 15:43:13 -0500

    Add 3.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,116 +1,99 @@
-'use strict';
+"use strict";
 
-const getChannelURL = require('ember-source-channel-url');
+const getChannelURL = require("ember-source-channel-url");
 
 module.exports = function() {
   return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary')
-  ]).then((urls) => {
+    getChannelURL("release"),
+    getChannelURL("beta"),
+    getChannelURL("canary")
+  ]).then(urls => {
     return {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              'ember': 'components/ember#lts-2-8'
-            },
-            resolutions: {
-              'ember': 'lts-2-8'
-            }
-          },
+          name: "ember-lts-2.16",
           npm: {
             devDependencies: {
-              'ember-source': null,
-              'ember-data': "~2.8.0"
+              "ember-source": "~2.16.0",
+              "ember-data": "~2.16.0"
             }
           }
         },
         {
-          name: 'ember-lts-2.12',
+          name: "ember-lts-2.18",
           npm: {
             devDependencies: {
-              'ember-source': '~2.12.0',
-              'ember-data': "~2.12.0"
+              "ember-source": "~2.18.0",
+              "ember-data": "~2.18.0"
             }
           }
         },
         {
-          name: 'ember-lts-2.16',
+          name: "ember-3.1",
           npm: {
             devDependencies: {
-              'ember-source': '~2.16.0',
-              'ember-data': "~2.16.0"
+              "ember-source": "~3.1.0",
+              "ember-data": "~3.1.0"
             }
           }
         },
         {
-          name: 'ember-lts-2.18',
+          name: "ember-lts-3.4",
           npm: {
             devDependencies: {
-              'ember-source': '~2.18.0',
-              'ember-data': "~2.18.0"
+              "ember-source": "~3.4.0",
+              "ember-data": "~3.4.0"
             }
           }
         },
         {
-          name: 'ember-3.1',
+          name: "ember-lts-3.8",
           npm: {
             devDependencies: {
-              'ember-source': '~3.1.0',
-              'ember-data': "~3.1.0"
+              "ember-source": "~3.8.0",
+              "ember-data": "~3.8.0"
             }
           }
         },
         {
-          name: 'ember-lts-3.4',
+          name: "ember-lts-3.12",
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0',
-              'ember-data': "~3.4.0"
+              "ember-source": "~3.12.0",
+              "ember-data": "~3.12.0"
             }
           }
         },
         {
-          name: 'ember-lts-3.8',
+          name: "ember-release",
           npm: {
             devDependencies: {
-              'ember-source': '~3.8.0',
-              'ember-data': "~3.8.0"
+              "ember-source": urls[0],
+              "ember-data": "latest"
             }
           }
         },
         {
-          name: 'ember-release',
+          name: "ember-beta",
           npm: {
             devDependencies: {
-              'ember-source': urls[0],
-              'ember-data': 'latest'
+              "ember-source": urls[1],
+              "ember-data": "beta"
             }
           }
         },
         {
-          name: 'ember-beta',
+          name: "ember-canary",
           npm: {
             devDependencies: {
-              'ember-source': urls[1],
-              'ember-data': 'beta'
+              "ember-source": urls[2],
+              "ember-data": "canary"
             }
           }
         },
         {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2],
-              'ember-data': 'canary'
-            }
-          }
-        },
-        {
-          name: 'ember-default',
+          name: "ember-default",
           npm: {
             devDependencies: {}
           }
